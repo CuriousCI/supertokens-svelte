5c2f08ce519c6a7cb286a49bff1d815264ac7648
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-06-05 16:25:11 +0200

    Add tailwindcss

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ pages: 'docs', assets: 'docs' }),
+		trailingSlash: 'always',
+		prerender: { enabled: true, crawl: true, default: true, entries: ['*'], onError: 'continue' }
 	}
 };
 
